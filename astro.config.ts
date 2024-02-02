b654e893440ce7f201bce0aade8048b0e992290b
JesseKoldewijn <jesse.koldewijn@denieuwezaak.nl>
2024-02-02 12:56:22 +0100

    fix: finally found why builds were failing, gotta love vscode's auto-refactor bs

---
+++ astro.config.ts
@@ -0,0 +1,17 @@
+import node from "@astrojs/node";
+import react from "@astrojs/react";
+import vercel from "@astrojs/vercel/serverless";
+import { defineConfig } from "astro/config";
+
+const isLocalPreview = process.env.LOCAL_PREVIEW === "true";
+
+// https://astro.build/config
+export default defineConfig({
+  output: "server",
+  adapter: isLocalPreview
+    ? node({
+        mode: "standalone",
+      })
+    : vercel(),
+  integrations: [react()],
+});
