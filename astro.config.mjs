1146cb8ac6dca743a126f482e6308a3f3f438c85
JesseKoldewijn <jesse.koldewijn@denieuwezaak.nl>
2024-02-02 12:38:48 +0100

    fix: added correct alias handling + sharp package

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,4 +4,11 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
+  vite: {
+    resolve: {
+      alias: {
+        "@/": "src/",
+      },
+    },
+  },
 });
