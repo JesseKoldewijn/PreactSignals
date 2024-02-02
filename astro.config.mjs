a6313e664c865058f73277935b320546a9275f40
JesseKoldewijn <jesse.koldewijn@denieuwezaak.nl>
2024-02-02 12:46:34 +0100

    fix: possible fix on build failure

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,11 @@
 import react from "@astrojs/react";
 import { defineConfig } from "astro/config";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
   vite: {
-    resolve: {
-      alias: {
-        "@/": "src/",
-      },
-    },
+    plugins: [tsconfigPaths()],
   },
 });
